864d26c7abc81721b74a5a5742d3c0d0ca7a45f1
nvencer98 <vencer98@gmail.com>
2022-12-23 09:26:38 -0800

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -4,38 +4,10 @@ import LightSwitch from "./components/LightSwitch"
 
 
 const App = () => {
-  const [lightSwitchStatus, setLightSwitchStatus] = useState("off")
-  const [backgroundStatus, setBackgroundStatus] = useState("white")
-
-
-
-
-
-  const handleClick = () => {
-  if (lightSwitchStatus === "off"){
-    setLightSwitchStatus("on")
-  } else {
-      setLightSwitchStatus("off")
-  }
-  lightSwitchStatus === "off"? setLightSwitchStatus("on") :setLightSwitchStatus("off")
-   
-  }
-
-
-
-
-
-
-
   return (
     <>
       <h1>Lightbulb Challenge</h1>
-      {/* <button onClick={handleClick}>on/off</button> */}
-      <LightSwitch lightSwitchStatus ={lightSwitchStatus}
-      backgroundStatus = {backgroundStatus}
-      /> 
-    
-  
+      <LightSwitch/> 
     </>
   )
 }
--- src/components/LightSwitch.js
+++ src/components/LightSwitch.js
@@ -3,29 +3,24 @@ import img1 from '../assets/lightOn.png'
 import img2 from '../assets/lightOff.png'
 
 
-const LightSwitch = (props) => {
-console.log(props)
+const LightSwitch = () => {
+  const [lightSwitchStatus, setLightSwitchStatus] = useState("off")
 
-const [lightSwitchStatus, setLightSwitchStatus] = useState("off")
+  const handleClick = () => {
+    lightSwitchStatus === "off"? setLightSwitchStatus("on") :setLightSwitchStatus("off")
+  }
 
-
-  
-    const handleClick = () => {
-      if (lightSwitchStatus === "off"){
-        setLightSwitchStatus("on")
-      } else {
-          setLightSwitchStatus("off")
-      }
-    }
   return (
     <>
-    
-      <div onClick={handleClick}style={{backgroundImage: lightSwitchStatus === "on"? "url("+img1+")": "url("+img2+")", backgroundSize: "contain"} } className="lightbulb" >{lightSwitchStatus} 
-      
+      <div 
+        onClick={handleClick}
+        style={{backgroundImage: lightSwitchStatus === "on"? "url("+img1+")": "url("+img2+")", backgroundSize: "contain"}} 
+        className="lightbulb"
+      >
+        {lightSwitchStatus} 
       </div> 
       
     </>
   )
-  }
-
+}
 export default LightSwitch
